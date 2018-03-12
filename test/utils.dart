661c410381e06586da695e4ffc97d72820ab41a0
Matan Lurey <matan@lurey.org>
2018-03-12 11:36:01 -0700

    Fix crash.

--- test/utils.dart
+++ test/utils.dart
@@ -7,7 +7,12 @@ import "package:test/test.dart";
 final Matcher throwsCastError = throwsA(new isInstanceOf<CastError>());
 
 /// A hack to determine whether we are running in a Dart 2 runtime.
-final bool isDart2 = (() {
-  bool isTypeArgString<T>(T arg) => T == String;
-  return isTypeArgString('');
-})();
+final bool isDart2 = _isTypeArgString('');
+bool _isTypeArgString<T>(T arg) {
+  try {
+    return T == String;
+  } catch (_) {
+    return false;
+  }
+}
+

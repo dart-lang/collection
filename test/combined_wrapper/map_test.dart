ca7154730c0309f9920699b6ace20f213bd5fbc4
Jacob MacDonald <jakemac@google.com>
2019-08-14 13:53:48 -0700

    update test to ensure the ordering contract is upheld

--- test/combined_wrapper/map_test.dart
+++ test/combined_wrapper/map_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:collection';
+
 import 'package:collection/collection.dart';
 import 'package:test/test.dart';
 
@@ -11,12 +13,15 @@ void main() {
   var map1 = const {1: 1, 2: 2, 3: 3};
   var map2 = const {4: 4, 5: 5, 6: 6};
   var map3 = const {7: 7, 8: 8, 9: 9};
-  var map4 = const {1: 1, 4: 4, 7: 7};
-  var concat = <int, int>{}
+  var map4 = const {1: -1, 2: -2, 3: -3};
+  var concat = SplayTreeMap<int, int>()
+    // The duplicates map appears first here but last in the CombinedMapView
+    // which has the opposite semantics of `concat`. Keys/values should be
+    // returned from the first map that contains them.
+    ..addAll(map4)
     ..addAll(map1)
     ..addAll(map2)
-    ..addAll(map3)
-    ..addAll(map4);
+    ..addAll(map3);
 
   // In every way possible this should test the same as an UnmodifiableMapView.
   common.testReadMap(

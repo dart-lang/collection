e230a7a710971784c4922746cd7afb49a48a0a33
creativecreatorormaybenot <19204050+creativecreatorormaybenot@users.noreply.github.com>
2020-06-07 17:15:08 +0000

    Update equality.dart

--- lib/src/equality.dart
+++ lib/src/equality.dart
@@ -407,7 +407,12 @@ class MultiEquality<E> implements Equality<E> {
 /// Deep equality on collections.
 ///
 /// Recognizes lists, sets, iterables and maps and compares their elements using
-/// deep equality as well.
+/// deep equality as well.  
+/// **Note**: the maximum depth for these comparisons is 1! This means that the
+/// comparison will fail when a sub collection in the collections to be compared
+/// contains another collection. For example, if you want to compare a list
+/// containing a list containing another list, [DeepCollectionEquality] will not
+/// be able to deeply compare the nested lists.
 ///
 /// Non-iterable/map objects are compared using a configurable base equality.
 ///
